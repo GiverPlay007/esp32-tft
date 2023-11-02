9eb4f70c171b1d699f530d7e45b0f8e8aee28e14
GiverPlay007 <play.giver.007@gmail.com>
2023-11-01 21:13:26 -0300

    Add TFT_eSPI library and print hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft;
 
 void setup() {
-  // put your setup code here, to run once:
+  tft.init();
+  tft.setRotation(1);
+  
+  tft.fillScreen(TFT_BLACK);
+  tft.println("Hello, World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
